ad1c8e4199ec637d06ebae0ee21f5df6c009b776
sridevi1995 <sridevidurai.1995@gmail.com>
2023-02-13 12:26:20 +0530

    Travel domain

---
+++ src/test/java/GITTravelProject/GITTravelClass.java
@@ -0,0 +1,17 @@
+package GITTravelProject;
+
+public class GITTravelClass {
+	private void bus() {
+		System.out.println("Air Buses");
+
+	}
+	private void car() {
+		System.out.println("Breeze");
+
+	}
+	private void train() {
+		System.out.println("AC Coach");
+
+	}
+
+}
